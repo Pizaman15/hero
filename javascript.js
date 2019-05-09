db9e0e2aa1a5f42fbb0d41febb163fbff6299da0
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-05-09 10:35:48 -0400

    Looks better on to the next thing to do interactEnd

--- javascript.js
+++ javascript.js
@@ -25,175 +25,178 @@ var heroPackage = {image: "@",
 
 var heroState = 1;
 
-// 1 is to move
-// 2 is to interact
-// 3 is to attack
-function actkey(e){
-  if(e.key == 5){heroState ++;}
-  if(heroState > 3){heroState = 1;}
-  if (heroState == 2){
-    interactKey(e);
+  // 1 is to move
+  // 2 is to interact
+  // 3 is to attack
+  function actkey(e){
+    if(e.key == 5){heroState ++;}
+    if(heroState > 3){heroState = 1;}
+    if (heroState == 2){
+      interactKey(e);
+    }
+   if(heroState == 1){logkey(e);}
+   if(heroState == 3){
+  console.log(heroState);
+    }
   }
-  if(heroState == 1){logkey(e);}
-  if(heroState == 3){
-    console.log(heroState);
-  }
-}
 
-function keys(key){
-  var cord = dungeon._keyToMove(key);
-  if(dungeon.map.cell[cord.y][cord.x].open){
-    dungeon.hero.location = cord;
-    return true;
-  }
-  return false;
-}
+  function keys(key){
+   var cord = dungeon._keyToMove(key);
+    if(dungeon.map.cell[cord.y][cord.x].open){
+       dungeon.hero.location = cord;
+      return true;
+     }
+    return false;
+   }
 
-function interact(key){
-  var cord = dungeon._keyToMove(key);
-  return cord;
-}
+   function interact(key){
+    var cord = dungeon._keyToMove(key);
+     return cord;
+   }
+
+ document.addEventListener("keyup", actkey);
+  function interactKey(e){
+    if(e.key == "End" || e.key == "1"){
+      interactStarter(interact("1"));
+     drawMap();
+    }
+    if(e.key == "ArrowDown" || e.key == "2"){
+      interactStarter(interact("2"));
+     drawMap();
+    }
+    if(e.key == "PageDown" || e.key == "3"){
+      interactStarter(interact("3"));
+     drawMap();
+    }
+    if(e.key == "ArrowLeft" || e.key == "4"){
+      interactStarter(interact("4"));
+     drawMap();
+    }
+    if(e.key == "ArrowRight" || e.key == "6"){
+      interactStarter(interact("6"));
+     drawMap();
+    }
+    if(e.key == "Home" || e.key == "7"){
+      interactStarter(interact("7"));
+     drawMap();
+    }
+    if(e.key == "ArrowUp" || e.key == "8"){
+      interactStarter(interact("8"));
+     drawMap();
+    }
+    if(e.key == "PageUp" || e.key == "9"){
+      interactStarter(interact("9"));
+     drawMap();
+    }
+  }
 
 document.addEventListener("keyup", actkey);
-function interactKey(e){
-  if(e.key == "End" || e.key == "1"){
-  interactStarter(interact("1"));
-  drawMap();
-}
-  if(e.key == "ArrowDown" || e.key == "2"){
-      interactStarter(interact("2"));
-    drawMap();
-  }
-  if(e.key == "PageDown" || e.key == "3"){
-      interactStarter(interact("3"));
-      drawMap();
-  }
+  function logkey(e){
+    if(e.key == "End" || e.key == "1"){
+      if(keys("1")) {
+        updates.innerHTML = dungeon.hero.name + "moved southwest";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "ArrowDown" || e.key == "2"){
+      if(keys("2")){
+        updates.innerHTML = dungeon.hero.name + " moved south";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "PageDown" || e.key == "3"){
+      if(keys("3")){
+        updates.innerHTML = dungeon.hero.name + " moved south east";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
     if(e.key == "ArrowLeft" || e.key == "4"){
-        interactStarter(interact("4"));
-    drawMap();
-  }
-  if(e.key == "ArrowRight" || e.key == "6"){
-      interactStarter(interact("6"));
-    drawMap();
-  }
-  if(e.key == "Home" || e.key == "7"){
-      interactStarter(interact("7"));
-    drawMap();
-  }
-  if(e.key == "ArrowUp" || e.key == "8"){
-      interactStarter(interact("8"));
-    drawMap();
-  }
-  if(e.key == "PageUp" || e.key == "9"){
-      interactStarter(interact("9"));
+      if(keys("4")){
+        updates.innerHTML = dungeon.hero.name + " moved west";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "ArrowRight" || e.key == "6"){
+      if(keys("6")){
+        updates.innerHTML = dungeon.hero.name + " moved east";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "Home" || e.key == "7"){
+      if(keys("7")){
+        updates.innerHTML = dungeon.hero.name + " moved north west";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "ArrowUp" || e.key == "8"){
+      if(keys("8")){
+        updates.innerHTML = dungeon.hero.name + " moved north";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
+      }
+    if(e.key == "PageUp" || e.key == "9"){
+      if(keys("9")){
+        updates.innerHTML = dungeon.hero.name + " moved north east";
+      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
+        drawMap();
     }
-    drawMap();
-    }
+  }
 
-
-
-
-                document.addEventListener("keyup", actkey);
-                function logkey(e){
-                  if(e.key == "End" || e.key == "1"){
-                    if(keys("1")) {
-                    updates.innerHTML = dungeon.hero.name + "moved southwest";
-                  }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                  drawMap();
-                }
-                  if(e.key == "ArrowDown" || e.key == "2"){
-                    if(keys("2")){
-                      updates.innerHTML = dungeon.hero.name + " moved south";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                  if(e.key == "PageDown" || e.key == "3"){
-                    if(keys("3")){
-                        updates.innerHTML = dungeon.hero.name + " moved south east";
-                      }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                      drawMap();
-                  }
-                    if(e.key == "ArrowLeft" || e.key == "4"){
-                      if(keys("4")){
-                      updates.innerHTML = dungeon.hero.name + " moved west";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                  if(e.key == "ArrowRight" || e.key == "6"){
-                      if(keys("6")){
-                      updates.innerHTML = dungeon.hero.name + " moved east";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                  if(e.key == "Home" || e.key == "7"){
-                    if(keys("7")){
-                      updates.innerHTML = dungeon.hero.name + " moved north west";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                  if(e.key == "ArrowUp" || e.key == "8"){
-                    if(keys("8")){
-                      updates.innerHTML = dungeon.hero.name + " moved north";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                  if(e.key == "PageUp" || e.key == "9"){
-                    if(keys("9")){
-                      updates.innerHTML = dungeon.hero.name + " moved north east";
-                    }else{updates.innerHTML = dungeon.hero.name + " hit a wall";}
-                    drawMap();
-                  }
-                }
+  //List creates a list 1-9 for inventory
+  //0 is a cancel for it and will be in a
+  //list format 1-9 + 0 being cancel
   function list(inventory){
-  var num = 9;
-  var inv = "The Merch <BR><BR>";
+    var num = 9;
+    var inv = "The Merch <BR><BR>";
    for (var i = 1; i <= num; i++) {
      if(inventory[i] !== undefined){
         inv += i + ". " + inventory[i];
-    }
-    inv += "<BR>"
+     }
+    inv += "<BR>";
    }
-   return inv += "0. cancel"
-}
+   return inv += "0. cancel";
+  }
 
+  //Makes the inventory when collecting things
+  //using function list 1-9 and 0 being cancel
   function interactStarter(coordinates){
-      coordinates = Utils.typeCheck(coordinates, "obj","interactStarter")
-    var stuff = dungeon.map.cell[coordinates.y][coordinates.x].list;
-    var text = list(stuff);
-    if(stuff !== undefined){
+    coordinates = Utils.typeCheck(coordinates, "obj","interactStarter");
+   var stuff = dungeon.map.cell[coordinates.y][coordinates.x].list;
+   var text = list(stuff);
+     if(stuff !== undefined){
       document.getElementById("inventory").innerHTML = "loot bag <br>" + text;
     }else{
-      document.getElementById("inventory").innerHTML = "inventory is empty"
+      document.getElementById("inventory").innerHTML = "inventory is empty";
+    }
   }
-}
 
-var dungeon = new Dungeon;
-dungeon.initalizeDungeon(mapPackage);
-ctx.innerHTML = dungeon.displayDungeon();
-/* End of inital conditions. */
-
-/* drawMap()
-return: none. Simply an event handler to redraw the current map display.
-*/
-function drawMap(){
+  var dungeon = new Dungeon;
+  dungeon.initalizeDungeon(mapPackage);
   ctx.innerHTML = dungeon.displayDungeon();
+  /* End of inital conditions. */
 
-}
-document.getElementById("drwbtn").onclick = function(){drawMap(); }
+  /* drawMap()
+  return: none. Simply an event handler to redraw the current map display.
+  */
+  function drawMap(){
+    ctx.innerHTML = dungeon.displayDungeon();
 
-/* newMap(conditions)
-@param conditions: {object} a mapPackage object.
-@return: none.
-*/
-function newMap(conditions){
-  dungeon.initalizeDungeon(conditions);
-  dungeon._initHero(heroPackage);
-  dungeon._placeHero();
-  ctx.innerHTML = dungeon.displayDungeon();
-}
-document.getElementById("newbtn").onclick = function(){ newMap(mapPackage);}
-document.getElementById("blkmap").onclick = function(){ newMap(blankPackage);}
-document.getElementById("updates").onclick = function(){}
+  }
+  document.getElementById("drwbtn").onclick = function(){drawMap(); }
+
+  /* newMap(conditions)
+  @param conditions: {object} a mapPackage object.
+  @return: none.
+  */
+  function newMap(conditions){
+    dungeon.initalizeDungeon(conditions);
+    dungeon._initHero(heroPackage);
+    dungeon._placeHero();
+    ctx.innerHTML = dungeon.displayDungeon();
+  }
+  document.getElementById("newbtn").onclick = function(){ newMap(mapPackage);}
+  document.getElementById("blkmap").onclick = function(){ newMap(blankPackage);}
+  document.getElementById("updates").onclick = function(){}
 
 /*
 function addRoom(size, map){
